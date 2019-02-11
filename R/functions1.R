f46cd5b45f561edb79e9615ed086a035c221fc1c
kgaythorpe <k.gaythorpe@googlemail.com>
2019-02-11 11:53:27 +0000

    inital commit

---
+++ R/functions1.R
@@ -0,0 +1,25 @@
+#' Get the name of the latest version of a file or directory
+#'
+#' @param pattern Add the pattern that the file or directory should start with.
+#'                If the file is in another directory this should also have the file path
+#'
+#' @export
+#' @return latest_file_name
+#'
+get_latest_file  = function(pattern){
+
+
+  #get a list of those files
+  file_names = list.files(pattern = pattern)
+
+  #check there are soem files
+  if(length(file_names) == 0){
+    stop("No files of that pattern in that directory")
+  }
+
+  file_dates = file.info(file_names)$mtime
+
+  ind = which.max(file_dates)
+
+  return(list(latest_file_name = file_names[ind]))
+}
